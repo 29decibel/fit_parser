172bb787441fc979a35af9e11d125227cd5fbef3
29decibel <mikeli@fastmail.fm>
2022-04-07 15:48:03 -0700

    init

---
+++ lib/fit_parser.ex
@@ -0,0 +1,18 @@
+defmodule FitParser do
+  @moduledoc """
+  Documentation for `FitParser`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FitParser.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/fit_parser_test.exs
@@ -0,0 +1,8 @@
+defmodule FitParserTest do
+  use ExUnit.Case
+  doctest FitParser
+
+  test "greets the world" do
+    assert FitParser.hello() == :world
+  end
+end
